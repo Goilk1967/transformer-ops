00d09cf74fd0f26c69fa10fa3d8300734e757c1d
Ethan Miller <olxiwipectra@gmx.net>
2023-11-10 14:00:00 -0500

    Add optimized multi-head attention implementation

--- attention.py
+++ attention.py
@@ -1,41 +1,18 @@
 import torch
 import torch.nn as nn
 import math
-
 class MultiHeadAttention(nn.Module):
-    """
-    Standard multi-head attention module for transformer architectures.
-    Implements scaled dot-product attention across multiple heads.
-    """
     def __init__(self, d_model, num_heads):
-        super(MultiHeadAttention, self).__init__()
-        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
-        
-        self.d_model = d_model
-        self.num_heads = num_heads
+        super().__init__()
         self.d_k = d_model // num_heads
-        
-        self.w_q = nn.Linear(d_model, d_model)
-        self.w_k = nn.Linear(d_model, d_model)
-        self.w_v = nn.Linear(d_model, d_model)
-        self.w_o = nn.Linear(d_model, d_model)
-
+        self.h = num_heads
+        self.linears = nn.ModuleList([nn.Linear(d_model, d_model) for _ in range(4)])
     def forward(self, q, k, v, mask=None):
-        batch_size = q.size(0)
-        
-        # Linear projections
-        q = self.w_q(q).view(batch_size, -1, self.num_heads, self.d_k).transpose(1, 2)
-        k = self.w_k(k).view(batch_size, -1, self.num_heads, self.d_k).transpose(1, 2)
-        v = self.w_v(v).view(batch_size, -1, self.num_heads, self.d_k).transpose(1, 2)
-        
-        # Scaled dot-product attention
+        nbatches = q.size(0)
+        q, k, v = [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2) for l, x in zip(self.linears, (q, k, v))]
         scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(self.d_k)
-        if mask is not None:
-            scores = scores.masked_fill(mask == 0, -1e9)
-        
-        attn = torch.softmax(scores, dim=-1)
-        output = torch.matmul(attn, v)
-        
-        # Concatenate heads and final projection
-        output = output.transpose(1, 2).contiguous().view(batch_size, -1, self.d_model)
-        return self.w_o(output)
+        if mask is not None: scores = scores.masked_fill(mask == 0, -1e9)
+        p_attn = torch.softmax(scores, dim=-1)
+        x = torch.matmul(p_attn, v)
+        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
+        return self.linears[-1](x)
